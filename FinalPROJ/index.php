7b7e1f0fdb8dbecaf2498c86f001f50c3f86cde7
Henry Rafael Reyes Jr <hereyes16@gmail.com>
2022-05-17 11:55:54 -0400

    Add files via upload

---
+++ FinalPROJ/index.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+
+  <nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="Products.php">Products</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="register.php">Register</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="about.asp">About</a></li>
+      
+  </ul>
+</nav>
+  
+
+<body>
+  <div class="home-container">
+  <div class="row">
+    <div class="col-2">
+    <h1>Give Your Workout <br>
+      A New Style!</h1>
+      <div class="home_underline"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis metus auctor, eleifend puru</P>
+    </div>
+    
+    <div class="col-2">
+     <img src="images/gym-girl.jpg">
+    
+  </div>
+  </div>
+
+  
+</body>
+<footer>
+  <div class="copyright">
+    <p>&copy 2013 - Organisation</p>
+  </div>
+  <div class="social">
+    <a href="#" class="support">Contact Us</a>
+    <a href="#" class="face">f</a>
+    <a href="#" class="tweet">t</a>
+    <a href="#" class="linked">in</a>
+  </div>
+</footer>
+</html>
